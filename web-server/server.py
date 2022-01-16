158c95423fc2ef64c65a61202e7a23c2829459df
cbrxyz <me@cbrxyz.com>
2022-01-16 12:20:14 -0500

    Add binding to localhost, default file if none provided

--- web-server/server.py
+++ web-server/server.py
@@ -3,20 +3,24 @@ import sys
 
 # Relevant values
 SERVER_PORT = 33867
+DEFAULT_FILE = "example.html"
 
 # Prepare the web server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('', SERVER_PORT))
+server_socket.bind(('127.0.0.1', SERVER_PORT))
 server_socket.listen(3)
-print("Socket is listening and can be found at {}.".format(server_socket.getsockname()))
+sock_name = server_socket.getsockname()
+print("Socket is listening and can be found at http://{}:{}.".format(sock_name[0], sock_name[1]))
 
 while True:
     connection_socket, addr = server_socket.accept()
     print("Received connection from {} on port {}.".format(addr[0], addr[1]))
 
     # Attempt to retrieve requested file
-    file_name = connection_socket.recv(1024).split()[1]
+    file_name = connection_socket.recv(1024).split()[1] 
     file_name = file_name.decode('UTF-8')[1:]
+    if not len(file_name):
+        file_name = DEFAULT_FILE
     print("Attempting to retrieve {}...".format(file_name))
 
     try:
