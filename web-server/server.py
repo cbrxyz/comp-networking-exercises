4f8bbefbe6d44bd91412a8d91a5822d60fcb88ba
cbrxyz <me@cbrxyz.com>
2022-01-16 12:38:24 -0500

    Add helper function to send content to socket, plaintext option for 404 requests

--- web-server/server.py
+++ web-server/server.py
@@ -4,6 +4,7 @@ import sys
 # Relevant values
 SERVER_PORT = 33867
 DEFAULT_FILE = "example.html"
+USE_404 = False
 
 # Prepare the web server socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,6 +12,16 @@ server_socket.bind(('127.0.0.1', SERVER_PORT))
 server_socket.listen(3)
 sock_name = server_socket.getsockname()
 print("Socket is listening and can be found at http://{}:{}.".format(sock_name[0], sock_name[1]))
+
+def send_content_to_connection(connection_socket: socket.socket, content: str):
+    # Send the HTTP header
+    connection_socket.send("HTTP/1.1 200 OK\n\n".encode())
+    
+    # Send the file contents line by line
+    connection_socket.send(content.encode())
+
+    # End HTTP response
+    connection_socket.send("\r\n".encode())
 
 while True:
     connection_socket, addr = server_socket.accept()
@@ -21,28 +32,31 @@ while True:
     file_name = file_name.decode('UTF-8')[1:]
     if not len(file_name):
         file_name = DEFAULT_FILE
-    print("Attempting to retrieve {}...".format(file_name))
+        print("Attempting to retrieve {}... (default)".format(DEFAULT_FILE))
+    else:
+        print("Attempting to retrieve {}...".format(file_name))
 
     try:
         # Attempt to retrieve the file and send it to the user
         file = open(file_name)
         content = file.read()
 
-        # Send the HTTP header
-        connection_socket.send("HTTP/1.1 200 OK\n\n".encode())
-        
-        # Send the file contents line by line
-        connection_socket.send(content.encode())
-
-        # End HTTP response
-        connection_socket.send("\r\n".encode())
+        # Send file content to connection
+        send_content_to_connection(connection_socket, content)
 
         pass
 
     except IOError:
         # Send response message for file not found
         # TODO
-        pass
+        if USE_404:
+            connection_socket.send("HTTP/1.1 404 Not Found\n\n".encode())
+
+            connection_socket.send("\r\n".encode())
+
+        else:
+            # Send 404 Not Found text to connection
+            send_content_to_connection(connection_socket, "404 Not Found")
 
     # Terminate server after sending data
     server_socket.close()
